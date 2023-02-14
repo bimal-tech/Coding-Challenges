3bf76c9792d66126f2c0c1f16bf0385fd5cc47df
bimal-tech <bimaladhikari8158@gmail.com>
2023-02-14 21:34:47 +0545

    modified the comments

--- challenge1.php
+++ challenge1.php
@@ -8,7 +8,6 @@ Output :4 is power of 2
 If $n = 8$, then the binary representation of $n$ is 1000.
 
 To compute $n & (n-1)$, we first need to compute $n-1$, which is equal to 7 in this case. The binary representation of 7 is 0111.
-
 Next, we perform the bitwise AND operation between 1000 and 0111, as follows:
 
     1000 (n)
@@ -18,7 +17,11 @@ Next, we perform the bitwise AND operation between 1000 and 0111, as follows:
     ------
 The result of the bitwise AND operation is 0, which means that $n & (n-1)$ is equal to 0.
 
-Mathematically, we can interpret this as follows: the binary representation of $n$ has only one set bit, which is the fourth bit from the right (corresponding to the digit 8 in decimal notation). When we subtract 1 from $n$, we get a binary number that has all the bits to the right of this set bit flipped, and the set bit itself cleared. In this case, the resulting binary number is 0111, which has no bits in common with the binary representation of $n$. Therefore, the bitwise AND operation between $n$ and $n-1$ clears all the bits in $n$, leaving us with 0.
+Mathematically, we can interpret this as follows: the binary representation of $n$ has only one set bit, which is the fourth bit 
+from the right (corresponding to the digit 8 in decimal notation). When we subtract 1 from $n$, we get a binary number that has 
+all the bits to the right of this set bit flipped, and the set bit itself cleared. In this case, the resulting binary number 
+is 0111, which has no bits in common with the binary representation of $n$. Therefore, the bitwise AND operation between
+$n$ and $n-1$ clears all the bits in $n$, leaving us with 0.
 
 Since $n & (n-1) = 0$, we can conclude that 8 is a power of 2.
 */
